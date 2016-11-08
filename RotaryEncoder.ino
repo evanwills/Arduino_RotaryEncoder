ae7f76c850baa8ac6066cbaa7821670ceba84d47
Evan Wills <evan.wills@acu.edu.au>
2016-11-09 06:53:04 +1100

    added basic stuff to test RotaryEncoder class (still need to sort out how to manage min/max limitation

--- RotaryEncoder.ino
+++ RotaryEncoder.ino
@@ -1,14 +1,55 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+
+
 #include "RotaryEncoder.h"
 
-RotaryEncoder basicEncoder(30,32);
+Adafruit_SSD1306 OLEDScreen(4); //OLED_RESET
 
-SimpleButton btn(38);
-BtnRotaryEncoder btnEncoder(34,36, btn);
+RotaryEncoder encoder(49, 47);
+//
+//ToggleButton toggleButton(45);
+//BtnRotaryEncoder encoder(49, 47, toggleButton);
+
+
 
 void setup() {
-	btn.makePinModePullup();
+//	toggleButton.makePinModePullup();
+
+	OLEDScreen.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+	OLEDScreen.clearDisplay();
+
+	OLEDScreen.setTextColor(WHITE);
+	OLEDScreen.setTextSize(1);
 }
 
 void loop() {
-	long pos = btnEncoder.getPosition();
+	OLEDScreen.clearDisplay();
+
+	// Write "Hello" in small
+	OLEDScreen.setCursor(0, 0);
+	OLEDScreen.print("Testing"); //this copies some text to the screens memory
+	OLEDScreen.setCursor(16, 12);
+	OLEDScreen.print("BtnRotaryEncoder"); //this copies some text to the screens memory
+
+	OLEDScreen.setCursor(32, 40);
+	OLEDScreen.print("pos: ");
+
+	long pos = encoder.getPosition();
+
+	OLEDScreen.print(pos);
+
+	// printing encoder button state seems to break whole sketch
+//	OLEDScreen.setCursor(24, 54);
+//	OLEDScreen.print("btn: ");
+//
+//	int state = encoder.getState();
+//
+//	OLEDScreen.print(state);
+
+
+
+	OLEDScreen.display();
 }
